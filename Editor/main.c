88a0b59fa485fccdefef3034ed5eb1e2e2d8f6ca
KitCopo <marquizim963@gmail.com>
2025-01-23 10:03:32 -0300

    Version 0.0.0 Alpha

---
+++ Editor/main.c
@@ -0,0 +1,14 @@
+/*
+SUPORTA APENAS WINDOWS 
+INTUITO DE SER UTILIZADO PARA PROJETOS NA KAIROS GAME ENGINE 
+COMEÇO 21/01/2025 FIM &&/&&/&&
+*/
+
+#include <stdio.h> 
+
+int main()
+{ 
+  
+  printf("Hello World");
+  return 0;     
+}
